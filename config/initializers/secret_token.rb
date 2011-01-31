d9b05a137bd285ba2b5cd21f510b7a2d6665b3f7
Josh Sullivan <josh@dipperstove.com>
2011-01-30 17:34:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skunkbank::Application.config.secret_token = '87e2fe161b7ef44c7538e040280e50a13cec2ae48727f6d1c137c0e8fb9d31ada5e01db66b54d3ce0304991b21856e93a7c2ee77fab98e1eb9908a0fb77317f6'
